b98655b01b747f81d9dbfbca4163f20d037e6411
FaridSaberi <146018109+FaridSaberi@users.noreply.github.com>
2023-12-25 12:39:43 +0330

    Delete AdditionalFiles directory

--- AdditionalFiles/L21.m
+++
@@ -1,5 +0,0 @@
-function D = L21(Z)
-z=4*max(diag(Z*(Z')),1e-10);
-D = diag(sqrt(1./z));
-end
-
--- AdditionalFiles/makesimlilarity.m
+++
@@ -1,9 +0,0 @@
-function [L,D,S] = makesimlilarity(X,K)
-options = [];
-options.NeighborMode = 'KNN';
-options.k = K;
-options.WeightMode = 'Binary';
-S = constructW(X,options);
-D = diag(sum(S));
-L = D - S;
-end
